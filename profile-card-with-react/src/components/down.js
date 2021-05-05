4a407a462a81f49e78caf8bdec94ce70e48b455b
Akinpelu Oluwatimileyin <72369149+Olaoluwadunni@users.noreply.github.com>
2021-05-05 16:05:01 +0100

    Update down.js
    Responsiveness

--- profile-card-with-react/src/components/down.js
+++ profile-card-with-react/src/components/down.js
@@ -3,19 +3,19 @@ import React from 'react'
 function Down() {
     return (   
         <div className="down">
-            <div>
+            <div class="stats">
                 <ul>
                     <li className="name-stats">80K</li>
                     <li className="gray1">Followers</li>
                 </ul>
             </div>
-            <div>
+            <div class="stats">
                 <ul>
                     <li className="name-stats">803K</li>
                     <li className="gray1">Likes</li>
                 </ul>
             </div>
-            <div>
+            <div class="stats" id="statss">
                 <ul>
                     <li className="name-stats">1.4K</li>
                     <li className="gray1">Photos</li>
