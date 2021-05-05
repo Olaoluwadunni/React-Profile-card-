819676be8c64bacebcc3799abee4e943427a20b3
Akinpelu Oluwatimileyin <72369149+Olaoluwadunni@users.noreply.github.com>
2021-05-05 16:57:45 +0100

    Update down.js

--- profile-card-with-react/src/components/down.js
+++ profile-card-with-react/src/components/down.js
@@ -3,19 +3,19 @@ import React from 'react'
 function Down() {
     return (   
         <div className="down">
-            <div class="stats">
+            <div className="stats">
                 <ul>
                     <li className="name-stats">80K</li>
                     <li className="gray1">Followers</li>
                 </ul>
             </div>
-            <div class="stats">
+            <div className="stats">
                 <ul>
                     <li className="name-stats">803K</li>
                     <li className="gray1">Likes</li>
                 </ul>
             </div>
-            <div class="stats" id="statss">
+            <div className="stats" id="statss">
                 <ul>
                     <li className="name-stats">1.4K</li>
                     <li className="gray1">Photos</li>
